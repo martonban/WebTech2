852dc56ec5514805cc5ee45ebb7c3a3cfe0b2a09
Márton Bán <30813844+martonban@users.noreply.github.com>
2022-04-02 22:51:55 +0200

    Basic GUI
    with Angular Material

--- Webshop/src/app/app.module.ts
+++ Webshop/src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from "@angular/forms";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import {ProductCreateComponent} from "./products/products-create/product-create.component";
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from "@angular/material/toolbar";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HeaderComponent} from "./header/header.component";
+
+
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+    declarations: [
+        AppComponent,
+        ProductCreateComponent,
+      HeaderComponent
+    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Webshop/src/app/products/products-create/product-create.component.ts
@@ -0,0 +1,16 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'app-product-create',
+  templateUrl: '/product-create.component.html',
+  styleUrls: ['./post-create-component.css']
+
+})
+export class ProductCreateComponent{
+  enterdValue = '';
+  newProduct = 'No Content';
+
+  onAddProduct(){
+    this.newProduct= this.enterdValue;
+  }
+}
