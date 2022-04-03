6e27bfceacfee02c923e2ee98e864c3e55e2a5eb
Márton Bán <30813844+martonban@users.noreply.github.com>
2022-04-03 15:15:16 +0200

    rename

--- Webshop/src/app/products/products-create/product-create.component.ts
+++ Webshop/src/app/products/products-create/product-create.component.ts
@@ -3,7 +3,7 @@ import {Component} from "@angular/core";
 @Component({
   selector: 'app-product-create',
   templateUrl: '/product-create.component.html',
-  styleUrls: ['./post-create-component.css']
+  styleUrls: ['./post-create.component.css']
 
 })
 export class ProductCreateComponent{
