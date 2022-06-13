e2008604ef26a371307619cd07e794604862f937
Márton Bán <30813844+martonban@users.noreply.github.com>
2022-06-13 18:27:14 +0200

    vscode init

--- Webshop/src/app/app.module.ts
+++ Webshop/src/app/app.module.ts
@@ -7,19 +7,20 @@ import {MatCardModule} from '@angular/material/card';
 import {ProductCreateComponent} from "./products/products-create/product-create.component";
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatExpansionModule} from "@angular/material/expansion";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HeaderComponent} from "./header/header.component";
-
-
+import {ProductListComponent} from "./products/product-list/product-list.component";
 
 
 @NgModule({
     declarations: [
         AppComponent,
         ProductCreateComponent,
-      HeaderComponent
+        HeaderComponent,
+        ProductListComponent
     ],
   imports: [
     BrowserModule,
@@ -29,7 +30,8 @@ import {HeaderComponent} from "./header/header.component";
     MatInputModule,
     MatCardModule,
     MatButtonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Webshop/src/app/products/product-list/product-list.component.ts
@@ -0,0 +1,21 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector : 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent{
+
+  /*
+  products = [
+    {title:"Miért jó az OpenGL", content:"Az OpenGL egy nagyon gyors és használtó API az alacsony szintű grafika programozáshoz"},
+    {title:"Miért jó a Vulkan", content:"Az Vulkan egy nagyon gyors és használtó API az alacsony szintű grafika programozáshoz"},
+    {title:"Miért jó a Metal", content:"Mert a buzi Apple"},
+    {title:"Miért jó a three.js", content:"Mert a kurva nehéz a többi"},
+  ];
+  */
+
+  products = [];
+
+}
