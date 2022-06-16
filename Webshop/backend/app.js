1bd7dab6179ed30f2932609ca86863f88534ad61
Márton Bán <30813844+martonban@users.noreply.github.com>
2022-06-16 20:59:03 +0200

    First data friom server side

--- Webshop/backend/app.js
+++ Webshop/backend/app.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const app = express('./backend/app');
 
-app.use((req, res, next) => {
-  console.log("First middleware");
-  next();
-});
 
 
-app.use((req, res, next) => {
-  res.send("Hello from express");
+app.use('/api/product', (req, res, next) => {
+  const products = [
+    {id: 'rbjeigjo',
+    title:'Test001',
+    content: 'This is coming from the server'
+    },
+    {id: 'dbjsrow',
+    title:'Test002',
+    content: 'This is coming from the server second time'
+    }
+  ];
+  res.status(200).json({
+    message: 'Product fetched succesfully!',
+    product: products
+  });
 });
 
 module.exports = app;
