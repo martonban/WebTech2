7ffdcfd6cbe469e0e98486c6010656d1b4c34384
Márton Bán <30813844+martonban@users.noreply.github.com>
2022-06-18 16:40:34 +0200

    Connect/Post/Get from mongodb

--- Webshop/backend/app.js
+++ Webshop/backend/app.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const bodyParser =  require('body-parser');
+const mongoose = require('mongoose');
+
+
+const Product = require('./models/product');
 
 const app = express();
+
+mongoose.connect("mongodb+srv://martonban:Jelszo123@cluster0.f52vjwd.mongodb.net/?retryWrites=true&w=majority")
+.then(() => {
+  console.log('Connected to database!');
+})
+.catch(() => {
+  console.log('Connection failed');
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -20,8 +32,11 @@ app.use((req, res, next) => {
 });
 
 app.post('/api/product', (req, res, next) => {
-  const product = req.body;
-  console.log(product);
+  const product = new Product({
+    title: req.body.title,
+    content: req.body.content
+  });
+  product.save();
   res.status(201).json({
     message: 'Product added succesfully'
   });
@@ -29,19 +44,12 @@ app.post('/api/product', (req, res, next) => {
 
 
 app.get('/api/product', (req, res, next) => {
-  const products = [
-    {id: 'rbjeigjo',
-    title:'Test001',
-    content: 'This is coming from the server'
-    },
-    {id: 'dbjsrow',
-    title:'Test002',
-    content: 'This is coming from the server second time'
-    }
-  ];
-  res.status(200).json({
-    message: 'Product fetched succesfully!',
-    product: products
+  Product.find()
+  .then(documents => {
+    res.status(200).json({
+      message: 'Product fetched succesfully!',
+      product: documents
+    });
   });
 });
 
