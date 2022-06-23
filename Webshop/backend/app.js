30987d8f64b4ba6aabced8735baed3a62d2b51dc
Márton Bán <30813844+martonban@users.noreply.github.com>
2022-06-24 00:46:41 +0200

    User Post

--- Webshop/backend/app.js
+++ Webshop/backend/app.js
@@ -9,6 +9,7 @@ const mongoose = require('mongoose');
 
 
 const productsRouters = require("./routes/products");
+const userRoutes = require("./routes/user")
 
 const app = express();
 
@@ -38,5 +39,6 @@ app.use((req, res, next) => {
 });
 
 app.use("/api/product", productsRouters);
+app.use("/api/user", userRoutes);
 
 module.exports = app;
